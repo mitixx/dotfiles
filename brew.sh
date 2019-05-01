8250feacc968480d2b064eb4eaff2bb2d32cc649
未知 <haruokame05@yahoo.co.jp>
2019-05-01 15:12:33 +0900

    add:bash.sh

---
+++ brew.sh
@@ -0,0 +1,17 @@
+brew update
+brew upgrade
+
+brew install pandoc
+brew install chrome-cli
+brew install ffmpeg
+brew install git
+brew install git-lfs
+brew install node
+brew install heroku
+brew install heroku-node
+brew install msgpack
+brew install openssl
+brew install wget
+brew install tor
+brew install terminal-notifier
+brew install ruby
